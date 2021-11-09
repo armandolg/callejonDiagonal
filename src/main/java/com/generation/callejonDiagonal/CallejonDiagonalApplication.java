500ff71b30bf54ae6494a0379a83a48ad90d75d8
Sergio Borjon E <sergioborjon1@gmail.com>
2021-11-09 15:17:33 -0600

    Creación del repositorio

---
+++ src/main/java/com/generation/callejonDiagonal/CallejonDiagonalApplication.java
@@ -0,0 +1,13 @@
+package com.generation.callejonDiagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallejonDiagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallejonDiagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/callejonDiagonal/CallejonDiagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.callejonDiagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallejonDiagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
